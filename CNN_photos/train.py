74368306be9cc0acfc4d7917c5e06243c4f75c41
Mateusz <154259613+matimusial@users.noreply.github.com>
2023-12-26 21:07:39 +0100

    Update train.py

--- CNN_photos/train.py
+++ CNN_photos/train.py
@@ -9,18 +9,16 @@ X_train, y_train, X_test, y_test = read_data()
 
 X_train_transposed, X_test_transposed = makearray(X_train, X_test)
 
-print(y_train)
+model = Sequential()
+model.add(Conv2D(32, (4, 4), input_shape=(32, 32, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (4, 4), input_shape=(32, 32, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# model = Sequential()
-# model.add(Conv2D(32, (4, 4), input_shape=(32, 32, 3), activation='relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(32, (4, 4), input_shape=(32, 32, 3), activation='relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Flatten())
-# model.add(Dense(128, activation='relu'))
-# model.add(Dense(10, activation='softmax'))
-# model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-#
-# model.fit(X_train_transposed, y_train, epochs=5, validation_data=(X_test_transposed, y_test))
-#
-# _, final_accuracy = model.evaluate(X_test_transposed,  y_test, verbose=2)
+model.fit(X_train_transposed, y_train, epochs=5, validation_data=(X_test_transposed, y_test))
+
+_, final_accuracy = model.evaluate(X_test_transposed,  y_test, verbose=2)
